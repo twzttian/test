6f4873e75992990d6e2a2aa25dc4edb7eb262fec
twzttian <50189535+twzttian@users.noreply.github.com>
2019-05-13 16:06:29 +0800

    修改test文件
    修改操作

--- Test.java
+++ Test.java
@@ -2,7 +2,10 @@ class test01{
 
 public static void main (String[] args){
 
-System.out.println("hello github !")
+System.out.println("hello github !");
+  foreache(String string list){
+  system.out.println(string);
+  }
 }
 
 }
